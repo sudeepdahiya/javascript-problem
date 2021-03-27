71d59d693ad37a6f4037b7b7b5d8287d3ff86763
Sudeep Dahiya <sudeep.dahiya@go-mmt.com>
2021-03-27 15:32:58 +0530

    debounce function

---
+++ debounce.js
@@ -0,0 +1,24 @@
+
+
+function debounce(func, timmer) {
+  let timer = null
+  return function (...arg) {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func(...arg);
+      // func.call(null, ...arg);
+      // func.apply(null, arg);
+      // const newFunc = func.bind(null, ...arg);newFunc();
+    }, timmer)
+  }
+}
+
+const printHello = (name, age, dob) => {
+  console.log('hello world', name, age, dob)
+}
+
+const newPrint = debounce(printHello, 500)
+
+newPrint('sudeep', 17, 10)
+newPrint('dahiya', 12, 19)
+newPrint('yes yes', 97, 12)
