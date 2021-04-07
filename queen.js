01686edbac7cc8238704a2a7328f5de55d0d4bb2
Sudeep Dahiya <sudeep.dahiya@go-mmt.com>
2021-04-07 20:04:34 +0530

    queen udpate with 100% fast in leet code

--- queen.js
+++ queen.js
@@ -7,13 +7,7 @@ var solveNQueens = function (n) {
   let output = []
   checkfornext([], 0, n, output);
   // output as per requirment
-  return output.map(res => res.map(val => {
-    let str = "";
-    for (let i = 0; i < n; i++) {
-      str += val === i ? 'Q' : '.'
-    }
-    return str;
-  }));
+  return output.map(res => res.map(val => 'Q'.padStart(val+1, '.').padEnd(n,'.')));
 };
 
 function checkfornext(list, next, total, output) {
@@ -24,8 +18,7 @@ function checkfornext(list, next, total, output) {
       for(let indx = 0 ; indx < list.length; indx ++){
         let q = list[indx];
         let diff = next - indx;
-        let arr = [q + diff, q - diff];
-        if (arr.indexOf(i) !== -1) {
+        if (q + diff === i || q - diff === i) {
           possible = false;
           break;
         }
@@ -40,4 +33,4 @@ function checkfornext(list, next, total, output) {
   }
 }
 
-console.log(solveNQueens(5))
+console.log(solveNQueens(4))
